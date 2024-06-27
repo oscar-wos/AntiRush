f6668820e3588e75f6cc72becf87f190627da1b6
oscar <29248751+oscar-wos@users.noreply.github.com>
2024-06-27 11:13:03 +0100

    fix: submodule reference

--- src/Commands.cs
+++ src/Commands.cs
@@ -2,7 +2,6 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Commands;
-using CounterStrikeSharp.API.Modules.Utils;
 
 namespace AntiRush;
 
