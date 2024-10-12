7280c663081c182f7191cafdc1374d657e03c7d3
Oscar <29248751+oscar-wos@users.noreply.github.com>
2024-10-12 15:16:01 +0100

    fix: cs_gamerules

--- src/Events.cs
+++ src/Events.cs
@@ -11,8 +11,8 @@ public partial class AntiRush
         _roundStart = Server.CurrentTime;
         _bombPlanted = false;
 
-        var gameRules = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").First().GameRules!;
-        _warmup = gameRules.WarmupPeriod;
+        _gameRules ??= Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").First().GameRules!;
+        _warmup = _gameRules?.WarmupPeriod;
 
         foreach (var zone in _zones)
         {
